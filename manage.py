d0f88b26a47b25f2b8b6f29e59ca887b3c9cba2d
Jose <josemigueltecum@gmail.com>
2024-12-11 17:15:26 -0600

    Usuariot

--- manage.py
+++ manage.py
@@ -25,11 +25,3 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
